5fa99d63f8b396ff4399b9f83368b72c48a8f07c
Alamin <alaminsf40@gmail.com>
2021-11-25 08:18:29 -0800

    country

--- custom.js
+++ custom.js
@@ -1,27 +1,27 @@
-fetch('https://jsonplaceholder.typicode.com/comments')
-.then(json => json.json())
-.then(data => displayData(data))
+// fetch('https://jsonplaceholder.typicode.com/comments')
+// .then(json => json.json())
+// .then(data => displayData(data))
 
 
 
 
-const displayData = (data) => {
-    const body = document.getElementById('body')
-    for(data of data){
-        let tr = document.createElement('tr')
-        tr.innerHTML = `
+// const displayData = (data) => {
+//     const body = document.getElementById('body')
+//     for(data of data){
+//         let tr = document.createElement('tr')
+//         tr.innerHTML = `
         
-        <td>${data.id}</td>
-        <td>${data.email}</td>
-        <td>${data.body}</td>
+//         <td>${data.id}</td>
+//         <td>${data.email}</td>
+//         <td>${data.body}</td>
 
         
         
         
-        `;
-        body.appendChild(tr)
-    }
-}
+//         `;
+//         body.appendChild(tr)
+//     }
+// }
 
 // const api = () => {
 //     fetch('https://api.kanye.rest/')
@@ -36,3 +36,41 @@ const displayData = (data) => {
 //     q.innerHTML = `${data.quote} <br>`;
 //     hello.appendChild(q);
 // }
+
+
+const randomUser = () => {
+    fetch('http://bdapis.herokuapp.com/api/v1.1/districts')
+    .then(response => response.json())
+    .then(data => displayData(data))
+}
+
+const displayData = (data) => {
+    const tBody = document.getElementById('tbody');
+    // console.log(data)
+    const district = data.data;
+    var num = 1;
+    for(dis of district){
+        let tr = document.createElement('tr');
+        tr.innerHTML = `<td>${num}</td> <td>${dis.districtbn}</td>`;
+        tBody.appendChild(tr);
+        num++
+    }
+}
+
+randomUser();
+
+const country = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(data => displayCountry(data))
+}
+
+const displayCountry = (data) =>{
+    for(data of data){
+        console.log(data.name.common)
+    }
+}
+
+country()
+
+
